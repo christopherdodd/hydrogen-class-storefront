af380ecc0cd1d0320659a7f1778c3a0fe0bc6c6c
Christopher Dodd <christopherrdodd@gmail.com>
2022-09-16 16:46:58 +0800

    Bring in basic index route file for displaying shop data

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,3 +1,28 @@
+import { useShopQuery, CacheLong, gql } from "@shopify/hydrogen";
+
 export default function Home() {
-  return <div>Hello World</div>;
+
+  const data = useShopQuery({
+      query: SHOP_QUERY,
+      cache: CacheLong(),
+      preload: true,
+  });
+
+const { data: { shop } } = data;
+
+  return (
+    <div className="home-page container">
+        <h1>{shop.name}</h1>
+        <div>{shop.description}</div>
+    </div>
+  );
 }
+
+const SHOP_QUERY = gql`
+  query ShopInfo {
+    shop {
+      name
+      description
+    }
+  }
+`;
