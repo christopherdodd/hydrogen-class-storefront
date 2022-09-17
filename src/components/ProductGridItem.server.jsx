de74a5bed0c3ff70cca73622c73caf0839176df0
Christopher Dodd <christopherrdodd@gmail.com>
2022-09-17 11:48:27 +0800

    Create catalog route and product grid item component

---
+++ src/components/ProductGridItem.server.jsx
@@ -0,0 +1,23 @@
+import { Image, Money } from '@shopify/hydrogen';
+
+export default function ProductCard({ product }) {
+
+    const { priceV2: price, compareAtPriceV2: compareAtPrice} = product.variants?.nodes[0] || {};
+
+    const isDiscounted = compareAtPrice?.amount > price?.amount;
+
+    return (
+        <div className="product-grid-item">
+            <div className="image-container">
+                <Image alt={product.featuredImage.altText} data={product.featuredImage} />
+            </div>
+            <div className="product-grid-item-title">{product.title}</div>
+            <div className="product-grid-prices">
+                <Money withoutTrailingZeros data={price} />
+                {isDiscounted && (
+                    <Money withoutTrailingZeros className="product-compare-at-price" data={compareAtPrice} />
+                )}
+            </div>
+        </div>
+    )
+}
---
+++ src/routes/catalog.server.jsx
@@ -0,0 +1,59 @@
+import { useShopQuery, CacheLong, gql } from '@shopify/hydrogen';
+import { Suspense } from 'react';
+
+import Layout from '../components/Layout.server';
+import ProductCard from '../components/ProductGridItem.server';
+
+export default function Catalog() {
+
+    const data = useShopQuery({
+        query: QUERY,
+        cache: CacheLong(),
+        preload: true,
+    });
+
+    const { data: { products: { nodes } } } = data;
+
+    return (
+        <Layout>
+            <Suspense>
+                <div className="catalog-page container">
+                    <div className="product-grid">
+                        {nodes.map((product) => (
+                            <ProductCard product={product}></ProductCard>
+                        ))}
+                    </div>
+                </div>
+            </Suspense>
+        </Layout>
+    )
+}
+
+const QUERY = gql`
+query products {
+    products(first: 9) {
+      nodes {
+        title
+        handle
+        featuredImage {
+          url
+          altText
+          height
+          width
+        }
+        variants(first: 1) {
+          nodes {
+            priceV2 {
+              amount
+              currencyCode
+            }
+            compareAtPriceV2 {
+              amount
+              currencyCode
+            }
+          }
+        }
+      }
+    }
+  }
+`;
